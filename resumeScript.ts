5feca1dc72cef641d1ee9125ffa048a6cffa75d5
arslan1git <164442382+arslan1git@users.noreply.github.com>
2024-11-09 13:44:41 +0500

    Done

--- resumeScript.ts
+++ resumeScript.ts
@@ -5,20 +5,17 @@ skillsBtn?.addEventListener("click", () => {
   skill_section!.classList.toggle("hide");
 });
 
-
 let experience_section = document.getElementById("experience-section");
 let experienceBtn = document.getElementById("experienceBtn");
 experienceBtn?.addEventListener("click", () => {
   experience_section!.classList.toggle("hide");
 });
 
-
 let certification_section = document.getElementById("certifications-section");
 let certificationBtn = document.getElementById("certificationBtn");
 certificationBtn?.addEventListener("click", () => {
   certification_section!.classList.toggle("hide");
 });
-
 
 let education_section = document.getElementById("education-section");
 let educationBtn = document.getElementById("educationBtn");
