1639fb768c5a4c03b01beed95325319e764a7de3
arslan1git <164442382+arslan1git@users.noreply.github.com>
2024-11-09 13:01:56 +0500

    MileStone01

---
+++ resumeScript.ts
@@ -0,0 +1,27 @@
+let skill_section = document.getElementById("skills-section");
+let skillsBtn = document.getElementById("skillsBtn");
+
+skillsBtn?.addEventListener("click", () => {
+  skill_section!.classList.toggle("hide");
+});
+
+
+let experience_section = document.getElementById("experience-section");
+let experienceBtn = document.getElementById("experienceBtn");
+experienceBtn?.addEventListener("click", () => {
+  experience_section!.classList.toggle("hide");
+});
+
+
+let certification_section = document.getElementById("certifications-section");
+let certificationBtn = document.getElementById("certificationBtn");
+certificationBtn?.addEventListener("click", () => {
+  certification_section!.classList.toggle("hide");
+});
+
+
+let education_section = document.getElementById("education-section");
+let educationBtn = document.getElementById("educationBtn");
+educationBtn?.addEventListener("click", () => {
+  education_section!.classList.toggle("hide");
+});
